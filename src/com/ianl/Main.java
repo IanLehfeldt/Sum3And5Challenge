99e0ca050ebd67d269f8622a4fa42e2413f0d36e
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-06-06 11:55:54 -0500

    moving forward with a challenge to determine if a range of numbers are odd or even

--- src/com/ianl/Main.java
+++ src/com/ianl/Main.java
@@ -7,15 +7,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    //threeAndFiveSumChecker();
 
+    }
+
+    public static void threeAndFiveSumChecker () {
         for(int i=1; i <= 1000; i++) {
             if(i%3 == 0) {
                 if(i%5 == 0) {
+                    // Could also write if(( i%3 == 0 ) && ( i%5 == 0) {...}
                     numbersSummed++;
                     sumOfAllNumbers+=i;
                     System.out.println(i+" is divisible by 3 and 5, adding ..");
                     if(numbersSummed == MAX_NUMBERS_SUMMED) {
-                        System.out.println("Found "+ numbersSummed +" numbers divisible by 3 and 5, closing loop with: " + sumOfAllNumbers);
+                        System.out.println("Found "+ numbersSummed +" numbers divisible by 3 and 5, closing loop with a sum of: " + sumOfAllNumbers);
                         numbersSummed = 0;
                         sumOfAllNumbers = 0;
                         break;
@@ -23,5 +28,14 @@ public class Main {
                 }
             }
         }
+    }
+
+    public static boolean isOdd(int number) {
+        if(number > 0) {
+            if (number % 2 == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
