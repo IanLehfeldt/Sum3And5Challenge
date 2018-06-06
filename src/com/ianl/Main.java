648683d03da2ee5346f59f219f7fe05edf048dc5
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-06-06 11:43:53 -0500

    created a for loop that manipulates public variables and even included a constant to easily manipulate max numbers to sum out

--- src/com/ianl/Main.java
+++ src/com/ianl/Main.java
@@ -1,8 +1,27 @@
 package com.ianl;
 
 public class Main {
+    public static int numbersSummed = 0;
+    public static int sumOfAllNumbers = 0;
+    public static final int MAX_NUMBERS_SUMMED = 5;
 
     public static void main(String[] args) {
 	// write your code here
+
+        for(int i=1; i <= 1000; i++) {
+            if(i%3 == 0) {
+                if(i%5 == 0) {
+                    numbersSummed++;
+                    sumOfAllNumbers+=i;
+                    System.out.println(i+" is divisible by 3 and 5, adding ..");
+                    if(numbersSummed == MAX_NUMBERS_SUMMED) {
+                        System.out.println("Found "+ numbersSummed +" numbers divisible by 3 and 5, closing loop with: " + sumOfAllNumbers);
+                        numbersSummed = 0;
+                        sumOfAllNumbers = 0;
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
